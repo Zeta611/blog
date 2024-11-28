5401eb799ba2ef57d773ef82660ca3610b5ffe82
Jay Lee <jaeho.lee@snu.ac.kr>
2024-11-28 23:33:53 +0900

    :wrench: Rewrite /react-trace

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   pageExtensions: ["mdx", "md", "jsx", "js", "ts", "tsx"],
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/react-trace/:path*",
+        destination: "https://react-trace.netlify.app/:path*",
+      },
+      {
+        source: "/_next/:path*",
+        destination: "https://react-trace.netlify.app/_next/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
